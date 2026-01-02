6cfc8600473938ef9bf7e5126debda1aaf74c98e
KeshiEmmanuel <keshichidera@gmail.com>
2026-01-02 17:59:21 -0500

    renamed middleware to proxy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,31 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        // Matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          // CRITICAL: Replace with your actual Extension ID from chrome://extensions
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "chrome-extension://jhbeaacafadccgllhigceiakgigpjbio",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { type NextRequest } from "next/server";
 import { updateSession } from "./lib/supabase/middleware/updateSession";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   // This function refreshes the auth token and manages cookies
   return await updateSession(request);
 }
